ce1b4ff434806b261be9ecffe19ba2a3226b0e8b
Ivan Pop-Jovanov <ivanpopjovanov@yahoo.com>
2023-09-07 12:37:32 +0200

    Some completion code

--- AR-projekat-2023/Completion.cpp
+++ AR-projekat-2023/Completion.cpp
@@ -1,23 +1,61 @@
 #include "Completion.h"
 #include "Substitution.h"
 
-void renamePair(TermPtr fm1, TermPtr fm2)
+#include "Unification.h"
+
+void renamePair(Equality fm1, Equality fm2)
 {
 	int i = 0;
 
 	set<string> fvs1; 
-	fm1->getVars(fvs1);
+	fm1.l->getVars(fvs1);
+	fm1.r->getVars(fvs1);
 	Substitution s1;
 	for (auto var : fvs1) {
 		s1.subs[var] = Var("x" + std::to_string(++i));
 	}
-	s1.substitute(fm1);
+	fm1.l = s1.substitute(fm1.l);
+	fm1.r = s1.substitute(fm1.r);
 
 	set<string> fvs2;
-	fm2->getVars(fvs2);
+	fm2.l->getVars(fvs2);
+	fm2.r->getVars(fvs2);
 	Substitution s2;
 	for (auto var : fvs2) {
 		s2.subs[var] = Var("x" + std::to_string(++i));
 	}
-	s2.substitute(fm2);
+	fm2.l = s2.substitute(fm2.l);
+	fm2.r = s2.substitute(fm2.r);
+}
+
+void overlaps(TermPtr l1, TermPtr l2, vector<Substitution>& substitutions)
+{
+	if (l1->type == Term::Variable)
+		return;
+
+	Substitution s;
+	if (unify(l1, l2, s))
+		substitutions.push_back(s);
+
+	for (auto arg : l1->function.args) {
+		overlaps(arg, l2, substitutions);
+	}
+}
+
+vector<CriticalPair> criticalPairs(Equality eq1, Equality eq2)
+{
+	vector<Substitution> substitutions;
+	overlaps(eq1.l, eq2.l, substitutions);
+
+	vector<CriticalPair> cps;
+	for (auto s : substitutions) {
+		auto l1 = s.substitute(eq1.l);
+		auto r1 = s.substitute(eq1.r);
+		auto l2 = s.substitute(eq2.l);
+		auto r2 = s.substitute(eq2.r);
+
+
+	}
+
+	return cps;
 }
--- AR-projekat-2023/Completion.h
+++ AR-projekat-2023/Completion.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "Term.h"
 #include "Equality.h"
+#include "Substitution.h"
 
 struct CriticalPair {
 	Equality l, r;
 };
 
 void renamePair(TermPtr fm1, TermPtr fm2);
-void overlaps(Equality eq1, Equality eq2);
+void overlaps(TermPtr l1, TermPtr l2, vector<Substitution>& substitutions);
+vector<CriticalPair> criticalPairs(Equality eq1, Equality eq2);
